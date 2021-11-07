012469c49abf28e17540ce5c1bb5da49d4b483ba
Manu Reddy <reddybmanohar@gmail.com>
2021-11-07 18:42:13 +0530

    output 7

--- outputs.tf
+++ outputs.tf
@@ -1,51 +1,47 @@
-#output "abc" {
-#  value = "Hello"
-#}
-#
-#output "abc1" {
-#  value = "Hello\nWorld"
-#}
+output "abc1" {
+  value = "Hello\nWorld"
+}
 
-#variable "abc" {
-#  default = "100"
-#}
-#
-#output "abc" {
-#  value = var.abc
-#}
-#
-#variable "xyz" {}
-#
-#output "xyz" {
-#  value = var.xyz
-#}
+variable "abc" {
+  default = "100"
+}
 
-## String Data type
-#variable "sample1" {
-#  default = "Hello World"
-#}
-#
-## Number data type
-#variable "sample2" {
-#  default = 100
-#}
-#
-## Boolean Data type
-#variable "sample3" {
-#  default = true
-#}
-#
-#output "sample1" {
-#  value = "Value of sample1 = ${var.sample1}"
-#}
-#
-#output "sample2" {
-#  value = var.sample2
-#}
-#
-#output "sample3" {
-#  value = " Value of sample3 = ${var.sample3}"
-#}
+output "abc" {
+  value = var.abc
+}
+
+variable "xyz" {}
+
+output "xyz" {
+  value = var.xyz
+}
+
+# String Data type
+variable "sample1" {
+  default = "Hello World"
+}
+
+# Number data type
+variable "sample2" {
+  default = 100
+}
+
+# Boolean Data type
+variable "sample3" {
+  default = true
+}
+
+output "sample1" {
+  value = "Value of sample1 = ${var.sample1}"
+}
+
+output "sample2" {
+  value = var.sample2
+}
+
+output "sample3" {
+  value = " Value of sample3 = ${var.sample3}"
+}
 
 variable "samplex" {
   default =  [
