96b601a2aab94bc4dee1a0ca4edfc4639f46125d
Manu Reddy <reddybmanohar@gmail.com>
2021-11-07 18:20:21 +0530

    output 6

--- outputs.tf
+++ outputs.tf
@@ -20,29 +20,54 @@
 #  value = var.xyz
 #}
 
-# String Data type
-variable "sample1" {
-  default = "Hello World"
+## String Data type
+#variable "sample1" {
+#  default = "Hello World"
+#}
+#
+## Number data type
+#variable "sample2" {
+#  default = 100
+#}
+#
+## Boolean Data type
+#variable "sample3" {
+#  default = true
+#}
+#
+#output "sample1" {
+#  value = "Value of sample1 = ${var.sample1}"
+#}
+#
+#output "sample2" {
+#  value = var.sample2
+#}
+#
+#output "sample3" {
+#  value = " Value of sample3 = ${var.sample3}"
+#}
+
+variable "samplex" {
+  default =  [
+    "Hello",
+    1000,
+    true,
+    "World"
+  ]
 }
 
-# Number data type
-variable "sample2" {
-  default = 100
+variable "sampley" {
+  default =  {
+    string = "Hello",
+    number = 100,
+    boolean = true
+  }
 }
 
-# Boolean Data type
-variable "sample3" {
-  default = true
+output "samplex" {
+  value = "Value 1 = ${var.samplex[0]}, Value 3 = ${var.samplex[2]}"
 }
 
-output "sample1" {
-  value = "Value of sample1 = ${var.sample1}"
-}
-
-output "sample2" {
-  value = var.sample2
-}
-
-output "sample3" {
-  value = " Value of sample3 = ${var.sample3}"
+output "sampley" {
+  value = "String = ${var.sampley[string]}, Number = ${var.sampley[number]}, Boolean = ${var.sampley[boolean]}"
 }
