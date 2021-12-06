dac2f47d6acca87f16a8ff71b3c6866add3e7fd4
Manu Reddy <reddybmanohar@gmail.com>
2021-12-07 01:36:48 +0530

    main 2

--- examples/main.tf
+++ examples/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "sample" {
-  ami           = ami-0855cab4944392d0a
+  ami           = "ami-0855cab4944392d0a"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_sample.id]
 
