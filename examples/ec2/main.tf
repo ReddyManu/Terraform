e4c7f9141430690a5ed081c9a0044eb46cf0faa1
Manu Reddy <reddybmanohar@gmail.com>
2021-12-10 20:06:52 +0530

    conditions 3

--- examples/ec2/main.tf
+++ examples/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "sample" {
-  count                  = length(var.name)
+  count                  = 0
   ami                    = "ami-0855cab4944392d0a"
   instance_type          = var.instance_type == "" ? "t2.micro" : var.instance_type
   vpc_security_group_ids = [var.SGID]
